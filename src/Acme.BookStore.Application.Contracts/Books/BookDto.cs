f1f087c465d8a0dcc3942632173b5ca7e7435159
Manish <manish.shrestha@amniltech.com>
2024-05-03 10:05:45 +0545

    This is the BookStore application made by Abp framework

---
+++ src/Acme.BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Auditing;
+
+namespace Acme.BookStore.Books
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public BookType Type { get; set; }
+        public DateTime PublishDate { get; set; }
+        public float Price { get; set; }
+    }
+}
